2c3d843ea09c7eb7a621866815e604c5f809d67b
dbw16 <dbw16@hotmail.com>
2023-12-23 11:09:49 +1300

    use env

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-# This is a sample Python script.
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+
+import fastapi
+
+app = fastapi.FastAPI()
 
 
-def print_hi(name: str) -> None:
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == "__main__":
-    print_hi("PyCharm")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.get("/")
+def root() -> dict[str, str]:
+    return {"message": f"The env is {os.environ['ENV']}"}
